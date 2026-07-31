5d3968de587cb9b7ce7e7217d49fc483f7d1ba87
adhyadumralia0508-pixel <adhyadumralia0508@gmail.com>
2026-07-31 08:54:08 +0530

    Delete haahid.py

--- haahid.py
+++
@@ -1,9 +0,0 @@
-def hash_index(key, table_size):
-    return key % table_size
-
-key = int(input("Enter key: "))
-table_size = int(input("Enter table size: "))
-
-index = hash_index(key, table_size)
-
-print("Hash Index =", index)
